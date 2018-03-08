80aea522e7d74fad4594cbe56856655a8ec1d39e
TeachMeHoW <szymoncichy123@gmail.com>
2018-03-08 13:29:10 +0100

    merged scripts into one file

--- skrypty/script.rb
+++ skrypty/script.rb
@@ -9,13 +9,24 @@ files.each do |x|
   file_out = File.open(x, 'w')
 
 #DO STH
+  regex_outer = /\"kp-faktura\",,\"kaucja\sdo/
+  regex_inner = /kp-faktura/
+  buffer = "\"kp-faktura\",,\"kaucja do"
+  buffer.gsub!(regex_inner, "kaucja")
+  contents.gsub!(regex_outer, buffer)
+
   regex_outer = /\"kw-faktura\",,\"zwrot\skaucji/
   regex_inner = /kw-faktura/
   buffer = "\"kw-faktura\",,\"zwrot kaucji"
   buffer.gsub!(regex_inner, "KAUCJA ZWROT")
   contents.gsub!(regex_outer, buffer)
-#DONE
 
+  regex_outer = /ING-K\",,\"Karty/
+  regex_inner = /ING-K/
+  buffer = "ING-K\",,\"Karty"
+  buffer.gsub!(regex_inner, "ING-O")
+  contents.gsub!(regex_outer, buffer)
+#DONE
 
   file_out.puts contents
   file_out.close
